f994761c24593eaeb39de0dd3307aabed03d09bc
Knight-JXNU <1990785022@qq.com>
2016-12-12 14:31:47 +0800

    java8 变量作用域

---
+++ src/main/java/VariableScope/VariableScope.java
@@ -0,0 +1,20 @@
+package VariableScope;
+
+/**
+ * Created by Knight_JXNU on 2016/12/12.
+ */
+public class VariableScope {
+    final static String salutation = "Hello ";
+    public static void main(String[] args){
+        GreetingService greetingService = message ->
+                System.out.println(salutation+message);
+        greetingService.sayMessage("google!");
+
+    }
+    private interface GreetingService{
+        public void sayMessage(String message);
+
+        //如果接口中有多个方法，lambda表达式报错
+//        public void hello(String m);
+    }
+}
